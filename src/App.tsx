0a63345d403990ac2f056a3c90ef744f973101b1
Arun Kumar <kumar.arun.ynr@gmail.com>
2024-06-24 21:19:47 +0530

    Added random function with min and max fields.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  
+  let [min, setMin] = useState("");
+  let [max, setMax] = useState("");
+
+  const outputField = document.getElementById("output") as HTMLElement;
+
+  function generateRandom() {
+    const maxNumber = Number(max);
+    const random = Math.floor(Math.random() * maxNumber);
+    outputField.innerText = `${random}`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input id="field1" type="number" value={min} onChange={e => setMin(e.target.value)} placeholder="0"/>
+        <input id="field2" type="number" value={max} onChange={e => setMax(e.target.value)} placeholder="50"/>
+      </div>
+      <button onClick={generateRandom}>Generate</button>
+      <div id="output" />
+    </>
   );
 }
 
