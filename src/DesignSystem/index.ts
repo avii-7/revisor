f91337f0ac3f4bd13e0f112d5749b63d8c9c713a
Arun Kumar <kumar.arun.ynr@gmail.com>
2026-06-06 12:22:48 +0530

    Add design system.

---
+++ src/DesignSystem/index.ts
@@ -0,0 +1,13 @@
+export {
+  DESIGN_PALETTE_NAME,
+  designColors,
+  getDesignColorCssVariable,
+} from "./colors";
+export type { DesignColorName } from "./colors";
+
+export {
+  designFontFamilies,
+  designTypography,
+  getDesignTypographyCssVariable,
+} from "./typography";
+export type { DesignTypographyName } from "./typography";
